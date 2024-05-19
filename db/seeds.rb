8e16815c26a6bc938fea1d02351604f10bacf90b
Laurent Sercomanens <laurent.sercomanens@gmail.com>
2024-05-19 15:15:24 +0530

    add a migration for a url column and add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,32 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 35,
+  number_of_guests:2,
+  picture_url: 'https://images.unsplash.com/photo-1494526585095-c41746248156?w=1200'
+)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat Delhi',
+  address: '1 charpot road',
+  description: 'True luxury deluxe',
+  price_per_night: 75,
+  number_of_guests: 3,
+  picture_url: 'https://images.unsplash.com/photo-1494526585095-c41746248156?w=1200'
+)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat New York',
+  address: '110 5th street',
+  description: 'Make your life great again',
+  price_per_night: 110,
+  number_of_guests: 1,
+  picture_url: 'https://images.unsplash.com/photo-1494526585095-c41746248156?w=1200'
+)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat Berlin',
+  address: 'Shlafen straat 45',
+  description: 'live it up in the eastern part',
+  price_per_night: 66,
+  number_of_guests: 30,
+  picture_url: 'https://images.unsplash.com/photo-1494526585095-c41746248156?w=1200'
+)
